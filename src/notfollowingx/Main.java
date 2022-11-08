ba7bedfce12829087bf5a73615ddb8fb7c6e8aea
themw123 <marvin.w6@web.de>
2022-11-08 09:38:45 +0100

    n

--- src/notfollowingx/Main.java
+++ src/notfollowingx/Main.java
@@ -1,6 +1,8 @@
 package notfollowingx;
 
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import Instagram.Instagram;
 import Instagram.Person;
@@ -8,79 +10,78 @@ import Instagram.Person;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		String username = "***REMOVED***";
 		String password = "***REMOVED***!";
 
-		//get session from mongodb
-		Mongodb mdb = new Mongodb();		
+		// get session from mongodb
+		Mongodb mdb = new Mongodb();
+		mdb.getSessionDB();
 		String ds_user_id = mdb.getDs_user_id();
 		String sessionId = mdb.getSessionId();
-		//check session
+		// check session
 		Instagram instagram = new Instagram();
 		instagram.connect("session", sessionId, ds_user_id);
-		if(!instagram.getSessionIdValid()) {
+		if (!instagram.getSessionIdValid()) {
 			instagram.connect("login", username, password);
-			//set new session
+			// set new session
 			mdb.setSessionDB(instagram.getSessionId());
 		}
-		//check session again
-		if(!instagram.getSessionIdValid()) {
+		// check session again
+		if (!instagram.getSessionIdValid()) {
 			System.out.println("Session still not valid!");
 			return;
 		}
-		
-		//get old follower
+
+		// get old follower
 		ArrayList<Person> followersOld = mdb.getFollowerDB();
-		
-		//get new followers
+
+		// get new followers
 		instagram.setFollowingAndFollowers("followers");
 		ArrayList<Person> followersNew = instagram.getFollowers();
-		//update followers in mongodb
+		// update followers in mongodb
 		mdb.setFollowerDB(followersNew);
-		
-		//compare old with new
+
+		// compare old with new
 		ArrayList<Person> notFollowAnymore = new ArrayList<Person>();
-		for(Person old : followersOld) {
+		for (Person old : followersOld) {
 			boolean still = false;
-			for(Person neww : followersNew) {
-				if(old.getId() == neww.getId()) {
+			for (Person neww : followersNew) {
+				if (old.getId() == neww.getId()) {
 					still = true;
 					break;
 				}
 			}
-			if(!still) {
+			if (!still) {
 				notFollowAnymore.add(old);
 			}
 		}
-		
-		
-		if(notFollowAnymore.isEmpty()) {
-	        System.out.println("No one has left you.");
+
+		if (notFollowAnymore.isEmpty()) {
+			System.out.println("No one has left you.");
 			return;
 		}
-		//send whatsapp
+		// send whatsapp
 		Whatsapp w = new Whatsapp();
 		String textWhatsapp = null;
 		String textConsole = null;
 
-        if(notFollowAnymore.size() == 1) {
-        	textWhatsapp = "Folgende%20Person%20folgt%20dir%20seit%20der%20letzten%20%C3%9Cberpr%C3%BCfung%20nicht%20mehr%3A%0A";
-        	textConsole = "Folgende Person folgt dir seit der letzten Ueberpruefung nicht mehr:";
-        }
-        else {
-        	textWhatsapp = "Folgende%20Personen%20folgen%20dir%20seit%20der%20letzten%20%C3%9Cberpr%C3%BCfung%20nicht%20mehr%3A%0A";
-        	textConsole = "Folgende Personen folgen dir seit der letzten Ueberpruefung nicht mehr: \n ";
-        }
-        
-        for(Person person : notFollowAnymore) {
-        	textWhatsapp = textWhatsapp + person.getName() + "%0A";  
-        	textConsole = textConsole + person.getName() + "\n";  
+		if (notFollowAnymore.size() == 1) {
+			textWhatsapp = "Folgende%20Person%20folgt%20dir%20seit%20der%20letzten%20%C3%9Cberpr%C3%BCfung%20nicht%20mehr%3A%0A";
+			textConsole = "Folgende Person folgt dir seit der letzten Ueberpruefung nicht mehr:";
+		} else {
+			textWhatsapp = "Folgende%20Personen%20folgen%20dir%20seit%20der%20letzten%20%C3%9Cberpr%C3%BCfung%20nicht%20mehr%3A%0A";
+			textConsole = "Folgende Personen folgen dir seit der letzten Ueberpruefung nicht mehr: \n ";
+		}
 
-        }
+		for (Person person : notFollowAnymore) {
+			textWhatsapp = textWhatsapp + person.getName() + "%0A";
+			textConsole = textConsole + person.getName() + "\n";
 
-        w.sendMessage(textWhatsapp);
-        System.out.println(textConsole);
+		}
+
+		w.sendMessage(textWhatsapp);
+		System.out.println(textConsole);
 
 	}
 
--- src/notfollowingx/Mongodb.java
+++ src/notfollowingx/Mongodb.java
@@ -1,6 +1,5 @@
 package notfollowingx;
 
-
 import org.bson.Document;
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
@@ -17,11 +16,10 @@ import Instagram.Person;
 
 import java.util.logging.Logger;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Level;
+
 public class Mongodb {
-	
 
 	private final String endpoint = "mongodb+srv://marv:***REMOVED***@cluster0.4ejve.mongodb.net/?retryWrites=true&w=majority";
 	private ConnectionString connectionString;
@@ -32,63 +30,62 @@ public class Mongodb {
 	private MongoCollection<Document> sessions;
 	private MongoCollection<Document> follower;
 
-	
 	private String sessionId;
 	private String ds_user_id;
-	
+
 	public Mongodb() {
+		// diable mongo logger
+		Logger mongoLogger = Logger.getLogger("org.mongodb.driver");
+		mongoLogger.setLevel(Level.SEVERE);
+
 		connectionString = new ConnectionString(endpoint);
 		settings = MongoClientSettings.builder()
-		        .applyConnectionString(connectionString)
-		        .build();
+				.applyConnectionString(connectionString)
+				.build();
 		mongoClient = MongoClients.create(settings);
 		databaseSession = mongoClient.getDatabase("session");
 		databaseNotFollowing = mongoClient.getDatabase("notFollowing");
 		sessions = databaseSession.getCollection("sessions");
 		follower = databaseNotFollowing.getCollection("follower");
-		Logger mongoLogger = Logger.getLogger("org.mongodb");
-		mongoLogger.setLevel(Level.WARNING);
-		getSessionDB();
 	}
-	
-	private void getSessionDB() {
-        Document doc = sessions.find(Filters.eq("name", "instagram")).first();
-        sessionId = doc.getString("sessionId");
-	    ds_user_id = doc.getString("ds_user_id");
-	}
-	
-	public void setFollowerDB(ArrayList<Person> followers) {
-       
-		//del all
-		follower.deleteMany(new Document());
-		
-		//add
-		List<Document> followersDocuments = new ArrayList<Document>();
-        for(Person follower : followers) {
-        	Document document = new Document().append("name", follower.getName()).append("id", follower.getId());
-        	followersDocuments.add(document);
-        }
-        follower.insertMany(followersDocuments);        
-	}
-	
-	public ArrayList<Person> getFollowerDB() {
-		
-		ArrayList<Person> followers = new ArrayList<Person>();
-		
-	    FindIterable<Document> iterDoc = follower.find();
-	    MongoCursor<Document> it = iterDoc.iterator();
-	    while (it.hasNext()) {    	
-	    	Document document = it.next();
-	    	Person person = new Person(document.getLong("id"), document.getString("name"));
-	    	followers.add(person);
-	    }
-	    return followers;
 
-        
+	public void getSessionDB() {
+		Document doc = sessions.find(Filters.eq("name", "instagram")).first();
+		sessionId = doc.getString("sessionId");
+		ds_user_id = doc.getString("ds_user_id");
 	}
-	
+
+	public void setFollowerDB(ArrayList<Person> followers) {
+
+		// del all
+		follower.deleteMany(new Document());
+
+		// add
+		List<Document> followersDocuments = new ArrayList<Document>();
+		for (Person follower : followers) {
+			Document document = new Document().append("name", follower.getName()).append("id", follower.getId());
+			followersDocuments.add(document);
+		}
+		follower.insertMany(followersDocuments);
+	}
+
+	public ArrayList<Person> getFollowerDB() {
+
+		ArrayList<Person> followers = new ArrayList<Person>();
+
+		FindIterable<Document> iterDoc = follower.find();
+		MongoCursor<Document> it = iterDoc.iterator();
+		while (it.hasNext()) {
+			Document document = it.next();
+			Person person = new Person(document.getLong("id"), document.getString("name"));
+			followers.add(person);
+		}
+		return followers;
+
+	}
+
 	public void setSessionDB(String sessionString) {
-	      sessions.updateOne(Filters.eq("name","instagram"), Updates.set("sessionId", sessionString));
+		sessions.updateOne(Filters.eq("name", "instagram"), Updates.set("sessionId", sessionString));
 	}
 
 	public String getSessionId() {
@@ -106,6 +103,5 @@ public class Mongodb {
 	public void setDs_user_id(String ds_user_id) {
 		this.ds_user_id = ds_user_id;
 	}
-	
-	
+
 }
